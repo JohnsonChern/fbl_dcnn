6ebaaa16ac64fd16beb45895b3efe0a87d072b86
Johnson Chern <cxhjonhson@gmail.com>
2017-02-06 14:48:10 +0800

    hhh

--- data_utils.py
+++ data_utils.py
@@ -2,11 +2,14 @@
 
 import xlrd
 import numpy as np
+from collections import defaultdict
 from bidict import namedbidict
 
 Vocab = namedbidict('Vocab', 'word', 'id')
 SRC_UNLABEL = "./data/src_unlabel.xlsx"
 TAR_UNLABEL = "./data/tar_unlabel.xlsx"
+SRC_TP = "./data/src_tp.csv"
+TAR_TP = "./data/tar_tp.csv"
 
 def read_excel(in_file, labeled=False):
     """
@@ -53,10 +56,42 @@ def merge_vocab(voc1, voc2):
 def get_vocab(docs):
     pass
 
-def build_input():
+def preprocess_data(data, label=None, padding_word='0'):
+    """
+    sort docs with their lengths and pad each sentence with '0'
+    """
+    if label is None:
+        label = [-1] * len(data)
+    lengths = []
+    sorted_index = defaultdict(list)
+    max_len = 0
+
+    for index in range(len(data)):
+        doc_len = len(data[index])
+        sorted_index[doc_len].append(index)
+        if doc_len > max_len:
+            max_len = doc_len
+    
+    processed_data = []
+    labels = []
+    for doc_len in sorted(sorted_index.keys()):
+        indexes = sorted_index[doc_len]
+        pad_num = max_len - doc_len
+        for index in indexes:
+            processed_data.append(data[index] + [padding_word] * pad_num)
+            labels.append(label[index])
+            lengths.append(doc_len)
+
+    processed_data = np.asarray(processed_data, dtype=np.int32)
+    return processed_data, labels, lengths
+
+def mon_lingual_input():
     src_unlabel = read_excel(SRC_UNLABEL)
     tar_unlabel = read_excel(TAR_UNLABEL)
-    src_vocab = getVocab(src_unlabel)
-    tar_vocab = getVocab(tar_unlabel)
-    vocab = merge_vocab(src_vocab, tar_vocab)
+    src_tp, _ = read_tp(SRC_TP)
+    tar_tp, vocab = read_tp(TAR_TP)
+    src_unlabel, _, src_len = preprocess_data(src_unlabel)
+    tar_unlabel, _, tar_len = preprocess_data(tar_unlabel)
 
+def mul_lingual_input():
+    pass
